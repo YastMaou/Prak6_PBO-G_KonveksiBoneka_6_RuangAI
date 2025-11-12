a9d971201de0d67705affa0993cbd76a71eb52ec
Riast Dwi Sanditya <riastdwisanditya@gmail.com>
2025-11-12 23:18:19 +0700

    Update KonveksiBoneka.java

--- KonveksiBoneka.java
+++ KonveksiBoneka.java
@@ -1,15 +1,17 @@
+
 public class KonveksiBoneka {
-    // CODE ANGGOTA 1 (KAMU)
     private String namaKonveksi;
     private String lokasi;
     private String jenisBoneka;
-
+    
+    // Constructor
     public KonveksiBoneka(String namaKonveksi, String lokasi, String jenisBoneka) {
         this.namaKonveksi = namaKonveksi;
         this.lokasi = lokasi;
         this.jenisBoneka = jenisBoneka;
     }
-
+    
+    // Method tampilkan info konveksi
     public void tampilkanInfoKonveksi() {
         System.out.println("=== INFO KONVEKSI ===");
         System.out.println("Nama Konveksi: " + namaKonveksi);
@@ -17,31 +19,8 @@ public class KonveksiBoneka {
         System.out.println("Jenis Boneka: " + jenisBoneka);
         System.out.println("--------------------");
     }
-
-    // CODE ANGGOTA 2 (TAMBAHAN)
+    
+    // STRUCTURE UNTUK STATIC NESTED CLASS (Anggota 2 akan lengkapi)
     public static class Produksi {
-        private String namaDesainer;
-        private int jumlahBoneka;
-        private int estimasiHari;
-        private double biayaProduksi;
-        
-        public Produksi(String namaDesainer, int jumlahBoneka, int estimasiHari, double biayaProduksi) {
-            this.namaDesainer = namaDesainer;
-            this.jumlahBoneka = jumlahBoneka;
-            this.estimasiHari = estimasiHari;
-            this.biayaProduksi = biayaProduksi;
-        }
-        
-        public void tampilkanProduksi() {
-            System.out.println("=== INFO PRODUKSI ===");
-            System.out.println("Nama Desainer: " + namaDesainer);
-            System.out.println("Jumlah Boneka: " + jumlahBoneka + " pcs");
-            System.out.println("Estimasi Hari: " + estimasiHari + " hari");
-            System.out.println("Biaya Produksi per Boneka: Rp " + biayaProduksi);
-            System.out.println("--------------------");
-        }
-        
-        public int getJumlahBoneka() { return jumlahBoneka; }
-        public double getBiayaProduksi() { return biayaProduksi; }
     }
 }
